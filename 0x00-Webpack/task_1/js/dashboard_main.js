584177f33b33515705a33f7a026fbfc7901132a4
Eric-Obeng <obengeric.3958@gmail.com>
2023-07-24 11:59:36 +0000

    task 0

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+const $ = required('jquery');
+const _ = required('lodash');
+
+$("<div id='logo'> </div>").appendTo('body');
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
+
+let counter = 0;
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} click on the button`;)
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
