279f175d9df1c7abf0856e92d492842387d98ca2
Eric-Obeng <obengeric.3958@gmail.com>
2023-07-26 10:25:19 +0000

    Modify the App

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -11,6 +11,21 @@ function App() {
       </header>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <form className="form">
+          <div className="form-email">
+            <label htmlFor="email">
+              Email
+            </label>
+            <input type="email" id="email" className="email-input input"/>
+          </div>
+          <div className="form-password">
+            <label htmlFor="password">
+              Password
+            </label>
+            <input type="password" id="password" className="password-input input" />
+          </div>
+          <button type="submit" className="form-btn">OK</button>
+        </form>
       </body>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
