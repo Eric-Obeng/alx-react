085fd9ae82878244ab130475a47b7d8548a6b348
Eric-Obeng <obengeric.3958@gmail.com>
2023-07-26 16:09:48 +0000

    Modify the Notification

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -4,5 +4,11 @@ const getFooterCopy = (isIndex) => {
     return isIndex === true ? 'Holberton School' :  'Holberton School main dashboard'
 }
 
-export { getFullYear, getFooterCopy }
+const getLatestNotification = () => {
+    return (
+        `<strong>Urgent required</strong> - complete by EOD`
+    )
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification }
 
